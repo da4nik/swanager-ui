da9019946b5669999e60050f4cc70a7413879c80
Maksim Stepanov <stepanov.ms@mail.ru>
2017-03-06 10:51:51 +0300

    Deploy

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 docker run --rm -w /app -v "$PWD":/app -e NODE_ENV=production node:6.9.5 bash -c "npm install && npm run build"
 
-docker build -t swanager-ui .
+docker build -t swanager-ui:latest .
