cb2ecda013fca70ffb02a7cb3678426cc808ac72
ZemtsovIgor <zemtsovigor@yahoo.com>
2017-04-04 13:04:47 +0300

    Important improvements

--- source/js/components/Inputs/InputField.jsx
+++ source/js/components/Inputs/InputField.jsx
@@ -7,15 +7,16 @@ class InputField extends React.Component {
     onChange: React.PropTypes.func,
     value: React.PropTypes.string,
     placeholder: React.PropTypes.string,
+    className: React.PropTypes.string,
   }
 
   render() {
-    const { title, inputType, onChange, value, placeholder } = this.props;
+    const { title, inputType, onChange, value, placeholder, className } = this.props;
     return (
       <div>
         <label htmlFor='email'>
           { title }
-          <input name='email' type={ inputType } onChange={ onChange } value={ value } placeholder={ placeholder } />
+          <input className={ className } name='email' type={ inputType } onChange={ onChange } value={ value } placeholder={ placeholder } />
         </label>
       </div>
     );
