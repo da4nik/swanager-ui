36ab1fdfe2f94de24cc28adc85393f57b3f8e5cc
ZemtsovIgor <zemtsovigor@yahoo.com>
2017-04-04 16:37:40 +0300

    Important improvements part5

--- source/js/views/Signup/index.jsx
+++ source/js/views/Signup/index.jsx
@@ -111,22 +111,20 @@ class Signup extends React.Component {
 
   render() {
     return (
-      <section className='container signin'>
-        <div className='signin-top'>
-          <h2 className='signin-top__header'>Create your account</h2>
+      <section className='container authBlock'>
+        <div className='signin'>
+          <h2 className='signin__header'>Create your account</h2>
           <form className='signin-form' onSubmit={ (event) => { this.onSignup(event); } }>
-            <InputField inputClass='signin-form__email' inputType='text' onChange={ (event) => { this.onEmailChanged(event); } } value={ this.state.email } placeholder='E-mail address' />
-            <InputField inputClass='signin-form__password' inputType='password' onChange={ (event) => { this.onPasswordChanged(event); } } value={ this.state.password } placeholder='Password' />
-            <InputField inputClass='signin-form__password' inputType='password' onChange={ (event) => { this.onPasswordConfirmationChanged(event); } } value={ this.state.passwordConfirmation } placeholder='Password Confirmation' />
+            <InputField inputClass='signin-form__input' inputType='text' onChange={ (event) => { this.onEmailChanged(event); } } value={ this.state.email } placeholder='E-mail address' />
+            <InputField inputClass='signin-form__input' inputType='password' onChange={ (event) => { this.onPasswordChanged(event); } } value={ this.state.password } placeholder='Password' />
+            <InputField inputClass='signin-form__input' inputType='password' onChange={ (event) => { this.onPasswordConfirmationChanged(event); } } value={ this.state.passwordConfirmation } placeholder='Password Confirmation' />
             { this.renderErrors() }
-            <button className='signin-form__submit' type='submit'>Sign up</button>
+            <button className='signin-form__button' type='submit'>Sign up</button>
           </form>
         </div>
-        <div className='signin-bottom'>
-          <p className='signin-bottom__goToSignUp'>
-            <span>Already registered? </span>
-            <Link to='/signin'>Sign In</Link>
-          </p>
+        <div className='goToSignUp'>
+          <span>Already registered? </span>
+          <Link to='/signin'>Sign In</Link>
         </div>
       </section>
     );
