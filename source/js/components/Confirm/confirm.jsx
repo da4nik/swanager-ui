458bc98afd515abe818c8175f2f94c4013c4bbf9
ZemtsovIgor <zemtsovigor@yahoo.com>
2017-04-26 19:32:57 +0300

    confirm changes

--- source/js/components/Confirm/confirm.jsx
+++ source/js/components/Confirm/confirm.jsx
@@ -4,5 +4,5 @@ import { createConfirmation } from 'react-confirm';
 const ConfirmPopUp = createConfirmation(confirmModal);
 
 export function confirm(confirmation, options = {}) {
-  return ConfirmPopUp({ confirmation, ...options });
+  return ConfirmPopUp({ confirmation, options });
 }
