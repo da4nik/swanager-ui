389e779a376860dde582d7ef2e12d92cb3623006
ZemtsovIgor <zemtsovigor@yahoo.com>
2017-05-10 17:37:17 +0300

    edit development and production env

--- config/development.js
+++ config/development.js
@@ -1,4 +1,4 @@
 export default {
-  apiURL: process.env.SWANAGER_API_URL,
-  wsURL: process.env.SWANAGER_WS_URL,
+  apiURL: process.env.SWANAGER_API_URL || 'http://localhost:4945/api/v1',
+  wsURL: process.env.SWANAGER_WS_URL || 'ws://localhost:4945/ws',
 };
