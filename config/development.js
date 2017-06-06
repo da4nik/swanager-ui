aaaaccff0f700d4a2fc25e59be4b7dc61ec6d95c
ZemtsovIgor <ZemtsovIgor@yahoo.com>
2017-06-06 19:03:00 +0300

    Env (#11)
    * ENV
    
    * edit development and production env
    
    * Managing configurations using environment variables
    
    * Added readme
    
    * Edit readme

--- config/development.js
+++ config/development.js
@@ -1,4 +1,4 @@
 export default {
-  apiURL: 'http://localhost:4945/api/v1',
-  wsURL: 'ws://localhost:4945/ws',
+  apiURL: process.env.myenv.SWANAGER_API_URL || 'http://localhost:4945/api/v1',
+  wsURL: process.env.myenv.SWANAGER_WS_URL || 'ws://localhost:4945/ws',
 };
